cd93a1819d8fdb91c8b32f2d2f79b88532f9f120
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 16:22:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  count = 0 
+  while count < names.length 
+  names.each do |names|
 end
 
 def summon_captain_planet# code an argument here
