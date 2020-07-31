bd9e05ffc790e5c1a50b17ac416d314496b040ed
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 16:52:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    cheese == cheese_types
   end
 end
